ea5ecbae09bf54963776ad43b51377d1b440da97
Cholak.AD <Cholak.AD@eka.ru>
2019-12-06 18:21:45 +0300

    Добавьте файлы проекта.

---
+++ Angl/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angl.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    // название 
+        public string Company { get; set; } // производитель
+        [Column(TypeName = "decimal(15, 2)")]
+        public decimal Price { get; set; }  // цена
+    }
+}
